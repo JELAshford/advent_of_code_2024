413563bbb82798e04b551f490193cd40ccede65d
James Ashford <ashfordj@rocketmail.com>
2024-12-04 19:32:07 +0000

    Alternative version with near-identical performance but a nicer underlying structure

--- src/day4.rs
+++ src/day4.rs
@@ -1,28 +1,36 @@
 use itertools::iproduct;
 
+pub struct Grid {
+    values: Vec<Vec<char>>,
+    width: isize,
+    height: isize,
+}
+
 #[aoc_generator(day4)]
-pub fn input_generator(input: &str) -> Vec<Vec<char>> {
-    input.lines().map(|line| line.chars().collect()).collect()
+pub fn input_generator(input: &str) -> Grid {
+    let vals: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+    Grid {
+        values: vals.clone(),
+        width: vals[0].len() as isize,
+        height: vals.len() as isize,
+    }
 }
 
 #[aoc(day4, part1)]
-pub fn solve_part1(input: &Vec<Vec<char>>) -> isize {
-    let num_rows = input.len() as isize;
-    let num_cols = input[0].len() as isize;
+pub fn solve_part1(input: &Grid) -> isize {
     let mut word_counts = 0;
-    for (y, x) in iproduct!(0..num_rows, 0..num_cols) {
-        if input[y as usize][x as usize] == 'X' {
+    for (y, x) in iproduct!(0..input.height, 0..input.width) {
+        if input.values[y as usize][x as usize] == 'X' {
             for direction in iproduct!(-1..=1, -1..=1) {
                 if direction != (0, 0) {
                     let slice: String = (0..=3)
                         .filter_map(|step| {
-                            let new_y = y + direction.0 * step;
-                            let new_x = x + direction.1 * step;
-                            if (new_y < 0) | (new_y >= num_rows) | (new_x < 0) | (new_x >= num_cols)
-                            {
+                            let dy = y as isize + direction.0 * step;
+                            let dx = y as isize + direction.0 * step;
+                            if (dy < 0) | (dy >= input.height) | (dx < 0) | (dx >= input.width) {
                                 return None;
                             } else {
-                                return Some(input[new_y as usize][new_x as usize]);
+                                return Some(input.values[dy as usize][dx as usize]);
                             }
                         })
                         .collect();
@@ -37,33 +45,19 @@ pub fn solve_part1(input: &Vec<Vec<char>>) -> isize {
 }
 
 #[aoc(day4, part2)]
-pub fn solve_part2(input: &Vec<Vec<char>>) -> isize {
-    let num_rows = input.len() as isize;
-    let num_cols = input[0].len() as isize;
+pub fn solve_part2(input: &Grid) -> isize {
     // Corners in order (tl, br, tr, bl)
     let corners: Vec<(isize, isize)> = vec![(-1, -1), (1, 1), (-1, 1), (1, -1)];
     let mut word_counts = 0;
-    for (y, x) in iproduct!(1..num_rows - 1, 1..num_cols - 1) {
-        if input[y as usize][x as usize] == 'A' {
+    for (y, x) in iproduct!(1..input.height - 1, 1..input.width - 1) {
+        if input.values[y as usize][x as usize] == 'A' {
             let corner_vals: String = corners
                 .iter()
-                .filter_map(|offset| {
-                    let new_y = y + offset.0;
-                    let new_x = x + offset.1;
-                    if (new_y < 0) | (new_y >= num_rows) | (new_x < 0) | (new_x >= num_cols) {
-                        return None;
-                    } else {
-                        return Some(input[new_y as usize][new_x as usize]);
-                    }
-                })
+                .map(|offset| input.values[(y + offset.0) as usize][(x + offset.1) as usize])
                 .collect();
-            // If none lost at the boundary
-            if corner_vals.len() == 4 {
-                // Check first and last pair each contain one "M" and one "S"
-                if (&corner_vals[0..2] == "MS") | (&corner_vals[0..2] == "SM") {
-                    if (&corner_vals[2..4] == "MS") | (&corner_vals[2..4] == "SM") {
-                        word_counts += 1;
-                    }
+            if (&corner_vals[0..2] == "MS") | (&corner_vals[0..2] == "SM") {
+                if (&corner_vals[2..4] == "MS") | (&corner_vals[2..4] == "SM") {
+                    word_counts += 1;
                 }
             }
         }
