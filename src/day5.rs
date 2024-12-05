2921e6680cb5967ecc45401a8b0b6a48dae92b72
James Ashford <ashfordj@rocketmail.com>
2024-12-05 11:35:53 +0000

    Day5 solution! Very neat, love the "sort_by" feature - so powerful!

--- src/day5.rs
+++ src/day5.rs
@@ -1,54 +1,64 @@
-use std::collections::HashSet;
+use hashbrown::{HashMap, HashSet};
 
+#[derive(Clone)]
 pub struct Problem {
-    rules: HashSet<(isize, isize)>,
     instructions: Vec<Vec<isize>>,
+    sorted_instructions: Vec<Vec<isize>>,
 }
 
 #[aoc_generator(day5)]
 pub fn input_generator(input: &str) -> Problem {
     let problem_parts: Vec<&str> = input.split("\n\n").collect();
+    let mut rules: HashMap<isize, HashSet<isize>> = HashMap::new();
+    for line in problem_parts[0].lines() {
+        let (from, to) = line.split_once("|").unwrap();
+        rules
+            .entry(from.parse().unwrap())
+            .or_default()
+            .insert(to.parse().unwrap());
+    }
+    let instructions: Vec<Vec<isize>> = problem_parts[1]
+        .lines()
+        .map(|line| {
+            line.split(",")
+                .map(|val| val.parse().unwrap())
+                .collect::<Vec<isize>>()
+        })
+        .collect();
+    let sorted_instructions: Vec<Vec<isize>> = instructions
+        .iter()
+        .map(|pages| {
+            let mut sorted_pages = pages.clone();
+            sorted_pages
+                .sort_by(|v1, v2| rules.entry(v2.clone()).or_default().contains(v1).cmp(&true));
+            sorted_pages
+        })
+        .collect();
+
     Problem {
-        rules: problem_parts[0]
-            .lines()
-            .map(|line| {
-                let vals = line
-                    .split("|")
-                    .map(|val| val.parse().expect("No map to int"))
-                    .collect::<Vec<isize>>();
-                (vals[0], vals[1])
-            })
-            .collect(),
-        instructions: problem_parts[1]
-            .lines()
-            .map(|line| {
-                line.split(",")
-                    .map(|val| val.parse().expect("No map to int"))
-                    .collect::<Vec<isize>>()
-            })
-            .collect(),
+        instructions,
+        sorted_instructions,
     }
 }
 
 #[aoc(day5, part1)]
 pub fn solve_part1(input: &Problem) -> isize {
-    input
-        .instructions
-        .iter()
-        .filter_map(|pages| {
-            for pos1 in 0..pages.len() {
-                for pos2 in (pos1 + 1)..pages.len() {
-                    if input.rules.contains(&(pages[pos2], pages[pos1])) {
-                        return None;
-                    }
-                }
-            }
-            return Some(pages[pages.len() / 2]);
+    let new_input = input.clone();
+    std::iter::zip(new_input.instructions, new_input.sorted_instructions)
+        .filter_map(|(pages, sorted_pages)| match pages == sorted_pages {
+            true => Some(pages[pages.len() / 2]),
+            false => None,
         })
         .sum()
 }
 
 #[aoc(day5, part2)]
 pub fn solve_part2(input: &Problem) -> isize {
-    todo!()
+    let new_input = input.clone();
+    std::iter::zip(new_input.instructions, new_input.sorted_instructions)
+        .filter_map(|(pages, sorted_pages)| match pages != sorted_pages {
+            true => Some(sorted_pages[pages.len() / 2]),
+            false => None,
+        })
+        .sum()
 }
