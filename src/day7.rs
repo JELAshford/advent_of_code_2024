d417e19d5b65ab9fc10a3a2e53b644fffc2d4e9e
James Ashford <ashfordj@rocketmail.com>
2024-12-08 10:06:28 +0000

    Huge runtime improvement using @lord_braleighs iterative discovery method! Remember this in future when trying lots of sequentially built options.

--- src/day7.rs
+++ src/day7.rs
@@ -1,16 +1,8 @@
-use itertools::repeat_n;
-use itertools::Itertools;
+use hashbrown::HashSet;
 
 pub struct Problem {
     target: isize,
     values: Vec<isize>,
-}
-
-#[derive(Clone, Debug)]
-pub enum Operator {
-    Plus,
-    Mult,
-    Concat,
 }
 
 #[aoc_generator(day7)]
@@ -30,40 +22,41 @@ pub fn input_generator(input: &str) -> Vec<Problem> {
         .collect()
 }
 
-fn can_find_target(target: isize, values: Vec<isize>, operators: Vec<Operator>) -> Option<usize> {
-    let num_operators = values.len() - 1;
-    for operations in repeat_n(operators, num_operators).multi_cartesian_product() {
-        let mut total = values[0].clone();
-        for (ind, operator) in operations.iter().enumerate() {
-            match operator {
-                Operator::Mult => total *= values[ind + 1],
-                Operator::Plus => total += values[ind + 1],
-                Operator::Concat => {
-                    let mut first_str = total.to_string();
-                    let second_str = values[ind + 1].to_string();
-                    first_str.push_str(&second_str);
-                    total = first_str.parse().unwrap()
-                }
+fn concat(a: isize, b: isize, base: isize) -> isize {
+    assert!(base >= 2);
+    a * isize::from(base).pow(b.ilog(base) + 1) + b
+}
+
+fn can_find_target(target: isize, values: Vec<isize>, use_concat: bool) -> Option<usize> {
+    // from @lord_braleigh
+    let mut numbers_we_can_make = HashSet::<isize>::new();
+    numbers_we_can_make.insert(0);
+    for new_number in values {
+        let mut new_numbers_we_can_make = HashSet::<isize>::new();
+        for old_number in numbers_we_can_make {
+            if old_number > target {
+                continue;
+            }
+            new_numbers_we_can_make.insert(old_number + new_number);
+            new_numbers_we_can_make.insert(old_number * new_number);
+            if use_concat {
+                new_numbers_we_can_make.insert(concat(old_number, new_number, 10));
             }
         }
-        if total == target {
-            return Some(target as usize);
-        }
+        numbers_we_can_make = new_numbers_we_can_make;
     }
-    return None;
+    if numbers_we_can_make.contains(&target) {
+        return Some(target as usize);
+    } else {
+        return None;
+    }
 }
 
 #[aoc(day7, part1)]
 pub fn solve_part1(input: &Vec<Problem>) -> usize {
     input
         .iter()
-        .filter_map(|problem| {
-            can_find_target(
-                problem.target,
-                problem.values.clone(),
-                vec![Operator::Mult, Operator::Plus],
-            )
-        })
+        .filter_map(|problem| can_find_target(problem.target, problem.values.clone(), false))
         .sum()
 }
 
@@ -71,12 +64,6 @@ pub fn solve_part1(input: &Vec<Problem>) -> usize {
 pub fn solve_part2(input: &Vec<Problem>) -> usize {
     input
         .iter()
-        .filter_map(|problem| {
-            can_find_target(
-                problem.target,
-                problem.values.clone(),
-                vec![Operator::Mult, Operator::Plus, Operator::Concat],
-            )
-        })
+        .filter_map(|problem| can_find_target(problem.target, problem.values.clone(), true))
         .sum()
 }
