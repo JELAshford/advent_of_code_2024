c98f1683aa45ff86fb643f3c070d7bf2d2a48137
James Ashford <ashfordj@rocketmail.com>
2024-12-08 15:20:23 +0000

    Cleanup! `isize` for all!

--- src/day8.rs
+++ src/day8.rs
@@ -3,23 +3,24 @@ use itertools::Itertools;
 
 #[derive(Debug)]
 pub struct StationGroup {
-    positions: HashMap<u8, Vec<(usize, usize)>>,
-    width: usize,
-    height: usize,
+    positions: HashMap<u8, Vec<(isize, isize)>>,
+    width: isize,
+    height: isize,
 }
 
 #[aoc_generator(day8)]
 pub fn input_generator(input: &str) -> StationGroup {
-    let height = input.lines().count();
-    let width = input.lines().peekable().peek().unwrap().len();
-    let mut positions = HashMap::<u8, Vec<(usize, usize)>>::new();
+    let height = input.lines().count() as isize;
+    let width = input.lines().peekable().peek().unwrap().len() as isize;
+    let mut positions = HashMap::<u8, Vec<(isize, isize)>>::new();
     for (y, line) in input.lines().enumerate() {
         for (x, val) in line.bytes().enumerate() {
             if val != b'.' {
+                let pos = (y as isize, x as isize);
                 _ = positions
                     .entry(val)
-                    .and_modify(|v| v.push((y, x)))
-                    .or_insert(vec![(y, x)])
+                    .and_modify(|v| v.push(pos))
+                    .or_insert(vec![pos])
             }
         }
     }
@@ -31,23 +32,23 @@ pub fn input_generator(input: &str) -> StationGroup {
 }
 
 fn generate_part1_antinodes(
-    station_positions: &[(usize, usize)],
+    station_positions: &[(isize, isize)],
     width: isize,
     height: isize,
-) -> Vec<(usize, usize)> {
-    let mut antinode_positions = Vec::<(usize, usize)>::new();
+) -> Vec<(isize, isize)> {
+    let mut antinode_positions = Vec::<(isize, isize)>::new();
     for ind1 in 0..station_positions.len() {
         let pos1 = station_positions[ind1];
         for ind2 in (ind1 + 1)..station_positions.len() {
             let pos2 = station_positions[ind2];
-            let (dy, dx) = ((pos2.0 - pos1.0) as isize, (pos2.1 - pos1.1) as isize);
-            let a1 = (pos1.0 as isize - dy, pos1.1 as isize - dx);
-            let a2 = (pos2.0 as isize + dy, pos2.1 as isize + dx);
+            let (dy, dx) = (pos2.0 - pos1.0, pos2.1 - pos1.1);
+            let a1 = (pos1.0 - dy, pos1.1 - dx);
+            let a2 = (pos2.0 + dy, pos2.1 + dx);
             if (a1.0 >= 0) & (a1.0 < height) & (a1.1 >= 0) & (a1.1 < width) {
-                antinode_positions.push((a1.0 as usize, a1.1 as usize));
+                antinode_positions.push(a1);
             }
             if (a2.0 >= 0) & (a2.0 < height) & (a2.1 >= 0) & (a2.1 < width) {
-                antinode_positions.push((a2.0 as usize, a2.1 as usize));
+                antinode_positions.push(a2);
             }
         }
     }
@@ -55,32 +56,30 @@ fn generate_part1_antinodes(
 }
 
 fn generate_part2_antinodes(
-    station_positions: &[(usize, usize)],
+    station_positions: &[(isize, isize)],
     width: isize,
     height: isize,
-) -> Vec<(usize, usize)> {
-    let mut antinode_positions = Vec::<(usize, usize)>::new();
+) -> Vec<(isize, isize)> {
+    let mut antinode_positions = Vec::<(isize, isize)>::new();
     if station_positions.len() <= 1 {
         return vec![];
     }
     for ind1 in 0..station_positions.len() {
         let pos1 = station_positions[ind1];
-        antinode_positions.push(pos1.clone());
         for ind2 in (ind1 + 1)..station_positions.len() {
             let pos2 = station_positions[ind2];
-            antinode_positions.push(pos2.clone());
 
-            let (dy, dx) = ((pos2.0 - pos1.0) as isize, (pos2.1 - pos1.1) as isize);
+            let (dy, dx) = (pos2.0 - pos1.0, pos2.1 - pos1.1);
             // one way ...
-            let mut new_pos = (pos1.0 as isize, pos1.1 as isize);
+            let mut new_pos = pos1.clone();
             while (new_pos.0 >= 0) & (new_pos.0 < height) & (new_pos.1 >= 0) & (new_pos.1 < width) {
-                antinode_positions.push((new_pos.0.clone() as usize, new_pos.1.clone() as usize));
+                antinode_positions.push(new_pos);
                 new_pos = (new_pos.0 - dy, new_pos.1 - dx);
             }
             // ... or another
-            let mut new_pos = (pos1.0 as isize, pos1.1 as isize);
+            let mut new_pos = pos1.clone();
             while (new_pos.0 >= 0) & (new_pos.0 < height) & (new_pos.1 >= 0) & (new_pos.1 < width) {
-                antinode_positions.push((new_pos.0.clone() as usize, new_pos.1.clone() as usize));
+                antinode_positions.push(new_pos);
                 new_pos = (new_pos.0 + dy, new_pos.1 + dx);
             }
         }
